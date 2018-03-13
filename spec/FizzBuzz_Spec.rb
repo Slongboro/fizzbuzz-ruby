8bd1575b3e2063b610b1cd384db261fe71c56db9
Alexander Russell <awjrussell@live.co.uk>
2018-03-13 14:12:49 +0000

    Refactored tests

--- spec/FizzBuzz_Spec.rb
+++ spec/FizzBuzz_Spec.rb
@@ -2,18 +2,19 @@ require_relative './spec_helper'
 
 describe FizzBuzz do
 
+    before :each do
+        @fizzbuzz = FizzBuzz.new
+    end
+
     it "When inputting 1 into FIzzBuzz it should return 1" do
-        fizzbuzz = FizzBuzz.new
-        expect(fizzbuzz.getFizzBuzz(1)).to eq(1)
+        expect(@fizzbuzz.getFizzBuzz(1)).to eq(1)
     end
 
     it "When inputting 3 into FizzBuzz it should return Fizz" do
-        fizzbuzz = FizzBuzz.new
-        expect(fizzbuzz.getFizzBuzz(3)).to eq("Fizz")
+        expect(@fizzbuzz.getFizzBuzz(3)).to eq("Fizz")
     end
 
     it "When inputting 5 into FizzBuzz it should return Buzz" do
-        fizzbuzz = FizzBuzz.new
-        expect(fizzbuzz.getFizzBuzz(5)).to eq("Buzz")
+        expect(@fizzbuzz.getFizzBuzz(5)).to eq("Buzz")
     end
 end

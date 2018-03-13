55cbe2450280cf57ba00a5ef44a0782626596399
Alexander Russell <awjrussell@live.co.uk>
2018-03-13 14:44:31 +0000

    fizzbuzz complete

--- spec/FizzBuzz_Spec.rb
+++ spec/FizzBuzz_Spec.rb
@@ -17,4 +17,19 @@ describe FizzBuzz do
     it "When inputting 5 into FizzBuzz it should return Buzz" do
         expect(@fizzbuzz.getFizzBuzz(5)).to eq("Buzz")
     end
+
+    it " When divisable by 3 and 5 it should return FizzBuzz" do
+        expect(@fizzbuzz.getFizzBuzz(15)).to eq("FizzBuzz")
+    
+end
+    it "When inputting 6 into FizzBuzz it should return Fizz" do
+        expect(@fizzbuzz.getFizzBuzz(6)).to eq("Fizz")
+    end
+
+    it "When inputting 10 into FizzBuzz it should return Buzz" do
+        expect(@fizzbuzz.getFizzBuzz(10)).to eq("Buzz")
+    end
+    it "When inputting 30 into FizzBuzz it vsould return FizzBuzz" do
+        expect(@fizzbuzz.getFizzBuzz(30)).to eq("FizzBuzz")
+    end
 end
